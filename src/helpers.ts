8d0a1cae0c355c39dc42db1dc5f480ccfc5c73a3
masnormen <nourmanhajar@gmail.com>
2022-01-13 13:36:53 +0700

    add super support

--- src/helpers.ts
+++ src/helpers.ts
@@ -25,69 +25,30 @@ export function handleOptions(request: Request) {
   });
 }
 
-export function generateSitemap() {
-  let sitemap = "<urlset xmlns='http://www.sitemaps.org/schemas/sitemap/0.9'>";
-  for (const each of slugs) {
-    sitemap += `<url><loc>https://${MY_DOMAIN}/${each}</loc></url>`;
-  }
-  sitemap += "</urlset>";
-  return sitemap;
+export async function generateSitemap(url: string, request: Request) {
+  console.log("URLLLL:", url);
+  const sitemap = await fetch(url, {
+    body: request.body,
+    headers: request.headers,
+    method: request.method,
+  });
+
+  const response = await sitemap.text;
+  return response.replace("nourman-hajar.super.site", MY_DOMAIN);
 }
 
 export class MetaRewriter {
   element(element: Element) {
-    if (PAGE_TITLE !== "") {
-      if (element.getAttribute("property") === "og:title" || element.getAttribute("name") === "twitter:title") {
-        element.setAttribute("content", PAGE_TITLE);
-      }
-      if (element.tagName === "title") {
-        element.setInnerContent(PAGE_TITLE);
-      }
-    }
-    if (PAGE_DESCRIPTION !== "") {
-      if (
-        element.getAttribute("name") === "description" ||
-        element.getAttribute("property") === "og:description" ||
-        element.getAttribute("name") === "twitter:description"
-      ) {
-        element.setAttribute("content", PAGE_DESCRIPTION);
-      }
-    }
-    if (element.getAttribute("property") === "og:url" || element.getAttribute("name") === "twitter:url") {
+    if (element.getAttribute("property") === "og:url") {
       element.setAttribute("content", MY_DOMAIN);
-    }
-    if (element.getAttribute("name") === "apple-itunes-app") {
-      element.remove();
     }
   }
 }
 
 export class HeadRewriter {
   element(element: Element) {
-    /* Google Font Options */
-    if (GOOGLE_FONT !== "") {
-      element.append(
-        `<link href="https://fonts.googleapis.com/css?family=${GOOGLE_FONT.replace(
-          " ",
-          "+"
-        )}:Regular,Bold,Italic&display=swap" rel="stylesheet">
-          <style>* { font-family: "${GOOGLE_FONT}" !important; }</style>`,
-        {
-          html: true,
-        }
-      );
-    }
     element.append(
-      `<style>
-        div.notion-topbar > div > div:nth-child(3) { display: none !important; }
-        div.notion-topbar > div > div:nth-child(4) { display: none !important; }
-        div.notion-topbar > div > div:nth-child(5) { display: none !important; }
-        div.notion-topbar > div > div:nth-child(6) { display: none !important; }
-        div.notion-topbar-mobile > div:nth-child(3) { display: none !important; }
-        div.notion-topbar-mobile > div:nth-child(4) { display: none !important; }
-        div.notion-topbar > div > div:nth-child(1n).toggle-mode { display: block !important; }
-        div.notion-topbar-mobile > div:nth-child(1n).toggle-mode { display: block !important; }
-        </style>`,
+      `<style>.super-badge { display: none !important; visibility: hidden !important; pointer-events: none !important; }</style>`,
       {
         html: true,
       }
@@ -95,136 +56,38 @@ export class HeadRewriter {
   }
 }
 
-export class BodyRewriter {
-  SLUG_TO_PAGE: Record<string, string>;
+export class DataRewriter {
+  buffer = "";
+  text(text: Text) {
+    this.buffer += text.text;
 
-  constructor(SLUG_TO_PAGE: Record<string, string>) {
-    this.SLUG_TO_PAGE = SLUG_TO_PAGE;
+    if (text.lastInTextNode) {
+      // We're done with this text node -- search and replace and reset.
+      text.replace(this.buffer.replaceAll("nourman-hajar.super.site", MY_DOMAIN));
+      this.buffer = "";
+    } else {
+      // This wasn't the last text chunk, and we don't know if this chunk
+      // will participate in a match. We must remove it so the client
+      // doesn't see it.
+      text.remove();
+    }
   }
+}
 
-  element(element: Element) {
-    element.append(
-      `<script>
-        window.CONFIG.domainBaseUrl = 'https://${MY_DOMAIN}';
-  
-        const SLUG_TO_PAGE = ${JSON.stringify(this.SLUG_TO_PAGE)};
-        const PAGE_TO_SLUG = {};
-        const slugs = [];
-        const pages = [];
-        const el = document.createElement('div');
-        let redirected = false;
-  
-        Object.keys(SLUG_TO_PAGE).forEach(slug => {
-          const page = SLUG_TO_PAGE[slug];
-          slugs.push(slug);
-          pages.push(page);
-          PAGE_TO_SLUG[page] = slug;
-        });
-        
-        function makeOpenFullPage() {
-          const cl = document.querySelectorAll('.notion-collection-item');
-          if (cl.length > 0) {
-            for (const element of cl) {
-                element.addEventListener('click', (e) => { e.stopPropagation(); });
-            }
-          }
-          const cla = document.querySelectorAll('.notion-collection-item a');
-          if (cla.length > 0) {
-            for (const element of cla) {
-                element.addEventListener('click', (e) => { e.stopPropagation(); });
-            }
-          }
-        }
-        function getPage() {
-          return location.pathname.slice(-32);
-        }
-        function getSlug() {
-          return location.pathname.slice(1);
-        }
-        function updateSlug() {
-          const slug = PAGE_TO_SLUG[getPage()];
-          if (slug != null) {
-            history.replaceState(history.state, '', '/' + slug);
-          }
-        }
-        function onDark() {
-          el.innerHTML = '<div title="Change to Light Mode" style="margin-left: auto; margin-right: 14px; min-width: 0px;"><div role="button" tabindex="0" style="user-select: none; transition: background 120ms ease-in 0s; cursor: pointer; border-radius: 44px;"><div style="display: flex; flex-shrink: 0; height: 14px; width: 26px; border-radius: 44px; padding: 2px; box-sizing: content-box; background: rgb(46, 170, 220); transition: background 200ms ease 0s, box-shadow 200ms ease 0s;"><div style="width: 14px; height: 14px; border-radius: 44px; background: white; transition: transform 200ms ease-out 0s, background 200ms ease-out 0s; transform: translateX(12px) translateY(0px);"></div></div></div></div>';
-          document.body.classList.add('dark');
-          __console.environment.ThemeStore.setState({ mode: 'dark' });
-        };
-        function onLight() {
-          el.innerHTML = '<div title="Change to Dark Mode" style="margin-left: auto; margin-right: 14px; min-width: 0px;"><div role="button" tabindex="0" style="user-select: none; transition: background 120ms ease-in 0s; cursor: pointer; border-radius: 44px;"><div style="display: flex; flex-shrink: 0; height: 14px; width: 26px; border-radius: 44px; padding: 2px; box-sizing: content-box; background: rgba(135, 131, 120, 0.3); transition: background 200ms ease 0s, box-shadow 200ms ease 0s;"><div style="width: 14px; height: 14px; border-radius: 44px; background: white; transition: transform 200ms ease-out 0s, background 200ms ease-out 0s; transform: translateX(0px) translateY(0px);"></div></div></div></div>';
-          document.body.classList.remove('dark');
-          __console.environment.ThemeStore.setState({ mode: 'light' });
-        }
-        function toggle() {
-          if (document.body.classList.contains('dark')) {
-            onLight();
-          } else {
-            onDark();
-          }
-        }
-        function addDarkModeButton(device) {
-          const nav = device === 'web' ? document.querySelector('.notion-topbar').firstChild : document.querySelector('.notion-topbar-mobile');
-          el.className = 'toggle-mode';
-          el.addEventListener('click', toggle);
-          nav.appendChild(el);
-          onLight();
-        }
-  
-        const observer = new MutationObserver(function() {
-          makeOpenFullPage();
-          if (redirected) return;
-          const nav = document.querySelector('.notion-topbar');
-          const mobileNav = document.querySelector('.notion-topbar-mobile');
-          if (nav && nav.firstChild && nav.firstChild.firstChild
-            || mobileNav && mobileNav.firstChild) {
-            redirected = true;
-            updateSlug();
-            addDarkModeButton(nav ? 'web' : 'mobile');
-            const onpopstate = window.onpopstate;
-            window.onpopstate = function() {
-              if (slugs.includes(getSlug())) {
-                const page = SLUG_TO_PAGE[getSlug()];
-                if (page) {
-                  history.replaceState(history.state, 'bypass', '/' + page);
-                }
-              }
-              onpopstate.apply(this, [].slice.call(arguments));
-              updateSlug();
-            };
-          }
-        });
-        observer.observe(document.querySelector('#notion-app'), {
-          childList: true,
-          subtree: true,
-        });
-  
-        const replaceState = window.history.replaceState;
-        window.history.replaceState = function(state) {
-          if (arguments[1] !== 'bypass' && slugs.includes(getSlug())) return;
-          return replaceState.apply(window.history, arguments);
-        };
-  
-        const pushState = window.history.pushState;
-        window.history.pushState = function(state) {
-          const dest = new URL(location.protocol + location.host + arguments[2]);
-          const id = dest.pathname.slice(-32);
-          if (pages.includes(id)) {
-            arguments[2] = '/' + PAGE_TO_SLUG[id];
-          }
-          return pushState.apply(window.history, arguments);
-        };
-  
-        const open = window.XMLHttpRequest.prototype.open;
-        window.XMLHttpRequest.prototype.open = function() {
-          arguments[1] = arguments[1].replace('${MY_DOMAIN}', 'www.notion.so');
-          return open.apply(this, [].slice.call(arguments));
-        };
-      </script>${CUSTOM_SCRIPT}`,
-      {
-        html: true,
-      }
-    );
+export class XMLRewriter {
+  buffer = "";
+  text(text: Text) {
+    this.buffer += text.text;
+
+    if (text.lastInTextNode) {
+      // We're done with this text node -- search and replace and reset.
+      text.replace(this.buffer.replaceAll("nourman-hajar.super.site", MY_DOMAIN));
+      this.buffer = "";
+    } else {
+      // This wasn't the last text chunk, and we don't know if this chunk
+      // will participate in a match. We must remove it so the client
+      // doesn't see it.
+      text.remove();
+    }
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,74 +1,51 @@
 import { MY_DOMAIN, SLUG_TO_PAGE, slugs } from "./config";
-import { BodyRewriter, generateSitemap, handleOptions, HeadRewriter, MetaRewriter } from "./helpers";
+import { DataRewriter, generateSitemap, handleOptions, HeadRewriter, MetaRewriter, XMLRewriter } from "./helpers";
 
-async function appendJavascript(res: Response, SLUG_TO_PAGE: Record<string, string>) {
+async function rewriteHTML(res: Response) {
   return new HTMLRewriter()
-    .on("title", new MetaRewriter())
-    .on("meta", new MetaRewriter())
     .on("head", new HeadRewriter())
-    .on("body", new BodyRewriter(SLUG_TO_PAGE))
+    .on("script#__NEXT_DATA__", new DataRewriter())
     .transform(res);
 }
 
 async function fetchAndApply(request: Request) {
-  if (request.method === "OPTIONS") {
-    return handleOptions(request);
-  }
   const url = new URL(request.url);
-  url.hostname = "www.notion.so";
 
   /* Robots.txt and Sitemap */
   if (url.pathname === "/robots.txt") {
-    return new Response(`Sitemap: https://${MY_DOMAIN}/sitemap.xml`);
+    return new Response(`User-agent: *
+Allow: /
+Sitemap: https://${MY_DOMAIN}/sitemap.xml`);
   }
+
+  url.hostname = "nourman-hajar.super.site";
+  url.protocol = "https:";
+  url.port = "";
+
   if (url.pathname === "/sitemap.xml") {
-    const response = new Response(generateSitemap());
-    response.headers.set("content-type", "application/xml");
-    return response;
-  }
-
-  /* Main Routes */
-  let response;
-
-  /* Serve JS files and apis */
-  if (url.pathname.startsWith("/app") && url.pathname.endsWith("js")) {
-    response = await fetch(url.toString());
-    const body = await response.text();
-    response = new Response(body.replace(/www.notion.so/g, MY_DOMAIN).replace(/notion.so/g, MY_DOMAIN), response);
-    response.headers.set("Content-Type", "application/x-javascript");
-    return response;
-  }
-  if (url.pathname.startsWith("/api")) {
-    // Forward API
-    response = await fetch(url.toString(), {
-      body: url.pathname.startsWith("/api/v3/getPublicPageData") ? null : request.body,
-      headers: {
-        "content-type": "application/json;charset=UTF-8",
-        "user-agent":
-          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36",
-      },
-      method: "POST",
+    let response = await fetch(url.toString(), {
+      body: request.body,
+      headers: request.headers,
+      method: request.method,
     });
     response = new Response(response.body, response);
-    response.headers.set("Access-Control-Allow-Origin", "*");
-    return response;
+    return new HTMLRewriter()
+      .on("*", new XMLRewriter())
+      .transform(response);
   }
 
-  /* Serve slugs */
-  if (slugs.indexOf(url.pathname.slice(1)) > -1) {
-    const pageId = SLUG_TO_PAGE[url.pathname.slice(1)];
-    return Response.redirect(`https://${MY_DOMAIN}/${pageId}`, 301);
-  }
-
-  response = await fetch(url.toString(), {
+  let response = await fetch(url.toString(), {
     body: request.body,
     headers: request.headers,
     method: request.method,
   });
   response = new Response(response.body, response);
-  response.headers.delete("Content-Security-Policy");
-  response.headers.delete("X-Content-Security-Policy");
-  return appendJavascript(response, SLUG_TO_PAGE);
+
+  if (url.pathname.endsWith(".js")) {
+    return response;
+  }
+
+  return rewriteHTML(response);
 }
 
 addEventListener("fetch", (event: FetchEvent) => {
