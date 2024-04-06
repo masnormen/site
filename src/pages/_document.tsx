8ff160e0adb8a45aec6982b029756fbbc6510731
Nourman Hajar <nourmanhajar@gmail.com>
2024-04-06 15:19:01 +0700

    fix: add analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,7 @@
 import { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
+
+const GTM_ID = "G-HJ6G6274PT";
 
 export default function Document() {
   return (
@@ -7,6 +10,19 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <link rel="preconnect" href="https://www.googletagmanager.com" />
+        <Script
+          id="load-tag"
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`}
+          strategy="beforeInteractive"
+        />
+        <Script id="setup-tag" async strategy="beforeInteractive">{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.GTM_ID}');
+        `}</Script>
       </body>
     </Html>
   );
