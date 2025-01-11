3a7a91345d8d2e32e1f3430149f9696cb4d4ece0
Nourman Hajar <nourmanhajar@gmail.com>
2025-01-11 12:56:46 +0700

    feat: init

---
+++ app/client.tsx
@@ -0,0 +1,18 @@
+/// <reference types="vinxi/types/client" />
+
+import { createRouter } from '@/router';
+import * as Sentry from '@sentry/react';
+import { StartClient } from '@tanstack/start';
+import { hydrateRoot } from 'react-dom/client';
+
+const router = createRouter();
+
+Sentry.init({
+  dsn: 'https://46616ef0d3464e4a9ff85122afeb3bf4@app.glitchtip.com/9789',
+  release: '1.0',
+  environment: 'development',
+  integrations: [Sentry.tanstackRouterBrowserTracingIntegration(router)],
+  tracesSampleRate: 1.0,
+});
+
+hydrateRoot(document, <StartClient router={router} />);
---
+++ app/router.tsx
@@ -0,0 +1,14 @@
+import { routeTree } from '@/routeTree.gen';
+import { createRouter as createTanStackRouter } from '@tanstack/react-router';
+
+export function createRouter() {
+  return createTanStackRouter({
+    routeTree,
+  });
+}
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: ReturnType<typeof createRouter>;
+  }
+}
