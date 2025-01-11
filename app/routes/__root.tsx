1d2ceccbd9e2f46743868c0a9972ddcc3fabb09d
Nourman Hajar <nourmanhajar@gmail.com>
2025-01-11 14:12:04 +0700

    feat: basic layout

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -1,3 +1,6 @@
+import Footer from '@/components/layouts/footer';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Outlet, ScrollRestoration, createRootRoute } from '@tanstack/react-router';
 import { Meta, Scripts } from '@tanstack/start';
 import type { ReactNode } from 'react';
@@ -13,17 +16,73 @@ export const Route = createRootRoute({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'TanStack Start Starter',
+        name: 'robots',
+        content: 'index,follow',
+      },
+      {
+        rel: 'canonical',
+        href: 'https://nourman.com/',
+      },
+      {
+        title: 'Nourman Hajar • Software Engineer',
+      },
+      {
+        name: 'description',
+        content: 'A blog on software engineering, web development, and miscellaneous tech stuff, by Nourman Hajar.',
+      },
+      {
+        name: 'twitter:card',
+        content: 'summary_large_image',
+      },
+      {
+        property: 'og:title',
+        content: 'Nourman Hajar • Software Engineer',
+      },
+      {
+        property: 'og:description',
+        content: 'A blog on software engineering, web development, and miscellaneous tech stuff, by Nourman Hajar.',
+      },
+      {
+        property: 'og:url',
+        content: 'https://nourman.com/',
+      },
+      {
+        property: 'og:image',
+        content: 'https://nourman.com/api/site.png',
+      },
+      {
+        property: 'og:image:type',
+        content: 'image/png',
+      },
+      {
+        property: 'og:image:width',
+        content: '1200',
+      },
+      {
+        property: 'og:image:height',
+        content: '630',
+      },
+      {
+        property: 'og:site_name',
+        content: 'Nourman Hajar • Software Engineer',
       },
     ],
+    links: [{ rel: 'icon', href: '/favicon.png' }],
   }),
   component: RootComponent,
 });
 
+// Create a client
+const queryClient = new QueryClient();
+
 function RootComponent() {
   return (
     <RootDocument>
-      <Outlet />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+      <Footer />
     </RootDocument>
   );
 }
@@ -32,6 +91,7 @@ function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
   return (
     <html>
       <head>
+        {process.env.NODE_ENV === 'development' && <script src="https://unpkg.com/react-scan/dist/auto.global.js" />}
         <Meta />
       </head>
       <body>
