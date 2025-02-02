76622de898ccc7496ef460a5c53e645897452727
Nourman Hajar <nourmanhajar@gmail.com>
2025-02-02 21:20:43 +0700

    fix: change copy path

--- app.config.ts
+++ app.config.ts
@@ -49,7 +49,7 @@ export default defineConfig({
               ? [
                   {
                     src: 'app/contents',
-                    dest: '.vercel/output/static',
+                    dest: '.vercel/output/functions/__nitro.func',
                   },
                 ]
               : []),
