ab2dff20fff84ffc87c9264929d6889019a6c5db
Nourman Hajar <nourmanhajar@gmail.com>
2025-02-03 22:20:38 +0700

    fix: env vars

--- app/utils/env.ts
+++ app/utils/env.ts
@@ -1,22 +1,19 @@
 import { z } from 'zod';
 
-const server = z.object({
+const any = z.object({
   APP_ENV: z.enum(['development', 'production']),
   SENTRY_DSN: z.string().url().optional(),
-});
-const serverVars = Object.keys(server.shape) as Array<
-  keyof typeof server.shape
->;
-
-const client = z.object({
   VITE_GA_ID: z.string().optional(),
   VITE_GTM_ID: z.string().optional(),
 });
-const clientVars = Object.keys(client.shape) as Array<
-  keyof typeof client.shape
->;
+const anyVarNames = Object.keys(any.shape) as Array<keyof typeof any.shape>;
 
-const merged = server.merge(client);
+const serverOnly = z.object({});
+// const serverOnlyVarNames = Object.keys(serverOnly.shape) as Array<
+//   keyof typeof serverOnly.shape
+// >;
+
+const merged = serverOnly.merge(any);
 
 // biome-ignore lint/nursery/noProcessEnv: needed
 let env = process.env as z.infer<typeof merged>;
@@ -25,7 +22,7 @@ let env = process.env as z.infer<typeof merged>;
 if (!!process.env.SKIP_ENV_VALIDATION == false) {
   const isServer = typeof window === 'undefined';
 
-  const parsed = isServer ? merged.safeParse(env) : client.safeParse(env);
+  const parsed = isServer ? merged.safeParse(env) : any.safeParse(env);
   if (parsed.success === false) {
     console.error(
       '❌ Invalid environment variables:',
@@ -37,7 +34,7 @@ if (!!process.env.SKIP_ENV_VALIDATION == false) {
   env = new Proxy(parsed.data, {
     get(target: z.infer<typeof merged>, prop: keyof typeof merged.shape) {
       if (typeof prop !== 'string') return undefined;
-      if (!isServer && (serverVars as string[]).includes(prop))
+      if (!isServer && !(anyVarNames as string[]).includes(prop))
         throw new Error(
           // biome-ignore lint/nursery/noProcessEnv: needed
           process.env.NODE_ENV === 'production'
