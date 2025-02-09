cb1c091f43159fed9f153d5caa90081f62b0e42e
Nourman Hajar <nourmanhajar@gmail.com>
2025-02-09 12:43:32 +0700

    fix: tracking code

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -111,12 +111,10 @@ export const Route = createRootRoute({
               src: `https://www.googletagmanager.com/gtag/js?id=${import.meta.env.VITE_GA_ID}`,
             },
             {
-              dangerouslySetInnerHTML: {
-                __html: `window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', '${import.meta.env.VITE_GA_ID}');`,
-              },
+              children: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', '${import.meta.env.VITE_GA_ID}');`,
             },
           ]
         : []),
@@ -124,13 +122,11 @@ export const Route = createRootRoute({
       ...(import.meta.env.VITE_GTM_ID
         ? [
             {
-              dangerouslySetInnerHTML: {
-                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','${import.meta.env.VITE_GTM_ID}');`,
-              },
+              children: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${import.meta.env.VITE_GTM_ID}');`,
             },
           ]
         : []),
