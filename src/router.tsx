9ba7d3e4da148a48a5bb0740608e669aa5bd0b46
Nourman Hajar <nourmanhajar@gmail.com>
2025-10-03 10:20:42 +0700

    feat: add default view transition

--- src/router.tsx
+++ src/router.tsx
@@ -13,6 +13,7 @@ export function createRouter() {
     scrollRestorationBehavior: 'instant',
     notFoundMode: 'root',
     defaultNotFoundComponent: NotFoundComponent,
+    defaultViewTransition: true,
   });
 }
 
